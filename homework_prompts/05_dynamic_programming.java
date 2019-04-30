670daf8db57456eba0a9619c5580ca6fd5efc6a0
niszeto <niszeto@gmail.com>
2019-04-30 13:04:06 -0700

    05 dynamic programming working recursive solution to coinSum problem

--- homework_prompts/05_dynamic_programming.java
+++ homework_prompts/05_dynamic_programming.java
@@ -1,3 +1,4 @@
+
 /**
  *  Homework 05 - Dynamic Programming: Tabulation/Memoization
  *
@@ -23,57 +24,62 @@ import java.util.*;
 class Problems {
 
   /*
-  *  1. Coin Sum
-  *
-  *          Given an array of coins and a target total, return how many
-  *          unique ways there are to use the coins to make up that total.
-  *
-  *  Input:  coins {Integer Array}, total {Integer}
-  *  Output: {Integer}
-  *
-  *
-  * Example:
-  *  Input:  [1,2,3], 4
-  *  Output: 4
-  *
-  *          1+1+1+1
-  *          1+3
-  *          1+1+2
-  *          2+2
-  *
-  *
-  *  Input:  [2,5,3,6], 10
-  *  Output: 5
-  *
-  *          2+3+5
-  *          5+5
-  *          2+3+2+3
-  *          2+2+6
-  *          2+2+3+3
-  *
-  *    Note: You have an unlimited number of each coin type. All coins in the
-  *          coin array will be unique
-  *          Order does not matter. Ex: One penny and one nickel to create six
-  *          cents is equivalent to one nickel and one penny
-  *
-  */
+   * 1. Coin Sum
+   *
+   * Given an array of coins and a target total, return how many unique ways there
+   * are to use the coins to make up that total.
+   *
+   * Input: coins {Integer Array}, total {Integer} Output: {Integer}
+   *
+   *
+   * Example: Input: [1,2,3], 4 Output: 4
+   *
+   * 1+1+1+1 1+3 1+1+2 2+2
+   *
+   *
+   * Input: [2,5,3,6], 10 Output: 5
+   *
+   * 2+3+5 5+5 2+3+2+3 2+2+6 2+2+3+3
+   *
+   * Note: You have an unlimited number of each coin type. All coins in the coin
+   * array will be unique Order does not matter. Ex: One penny and one nickel to
+   * create six cents is equivalent to one nickel and one penny
+   *
+   */
 
   // Time Complexity:
   // Auxiliary Space Complexity:
 
   public static int coinSum(int[] coins, int total) {
-    //YOUR WORK HERE
-    return -1;
+    return coinSumHelper(coins, total, 0);
+  }
+
+  private static int coinSumHelper(int[] coins, int total, int index) {
+
+    if (total == 0) {
+      return 1;
+    }
+
+    if (index >= coins.length) {
+      return 0;
+    }
+
+    int amountWithCoin = 0;
+    int differentWaysToMakeChange = 0;
+
+    while (amountWithCoin <= total) {
+      int remaining = total - amountWithCoin;
+      differentWaysToMakeChange = differentWaysToMakeChange + coinSumHelper(coins, remaining, index + 1);
+      amountWithCoin = amountWithCoin + coins[index];
+    }
+
+    return differentWaysToMakeChange;
   }
 
 }
 
-
-
-
-
 ////////////////////////////////////////////////////////////
-///////////////  DO NOT TOUCH TEST BELOW!!!  ///////////////
+/////////////// DO NOT TOUCH TEST BELOW!!! ///////////////
 ////////////////////////////////////////////////////////////
 
 // use the Main class to run the test cases
@@ -87,35 +93,36 @@ class Main {
 
   public static void main(String[] args) {
 
-    // instantiate the testing of each module by resetting count and printing title of module
-    int[] testCount = {0, 0};
+    // instantiate the testing of each module by resetting count and printing title
+    // of module
+    int[] testCount = { 0, 0 };
     System.out.println("Coin Sum Tests");
 
     // tests are in the form as shown
     assertTest(testCount, "should work for first example case", new Test() {
       public boolean execute() {
-        int output = Problems.coinSum(new int[]{1,2,3}, 4);
+        int output = Problems.coinSum(new int[] { 1, 2, 3 }, 4);
         return output == 4;
       }
     });
 
     assertTest(testCount, "should work for second example case", new Test() {
       public boolean execute() {
-        int output = Problems.coinSum(new int[]{2,5,3,6}, 10);
+        int output = Problems.coinSum(new int[] { 2, 5, 3, 6 }, 10);
         return output == 5;
       }
     });
 
     assertTest(testCount, "should work for a single coin", new Test() {
       public boolean execute() {
-        int output = Problems.coinSum(new int[]{2}, 10);
+        int output = Problems.coinSum(new int[] { 2 }, 10);
         return output == 1;
       }
     });
 
     assertTest(testCount, "should work for a single coin", new Test() {
       public boolean execute() {
-        int output = Problems.coinSum(new int[]{2}, 10);
+        int output = Problems.coinSum(new int[] { 2 }, 10);
         return output == 1;
       }
     });
@@ -123,7 +130,6 @@ class Main {
     // print the result of tests passed for a module
     System.out.println("PASSED: " + testCount[0] + " / " + testCount[1] + "\n\n");
   }
-
 
   // do not edit below, this is to wrap the test and check for exceptions
   private static void assertTest(int[] count, String name, Test test) {
@@ -135,7 +141,8 @@ class Main {
         pass = " true";
         count[0]++;
       }
-    } catch(Exception e) {}
+    } catch (Exception e) {
+    }
     String result = "  " + (count[1] + ")   ").substring(0, 5) + pass + " : " + name;
     System.out.println(result);
   }
